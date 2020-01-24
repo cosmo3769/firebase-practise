7b69184a01e4606b4b8458ad0507d78603389b2b
piyush-cosmo <53268607+piyush-cosmo@users.noreply.github.com>
2020-01-25 01:26:54 +0530

    backend

---
+++ practise-functions/functions/index.js
@@ -0,0 +1,25 @@
+const functions = require('firebase-functions');
+
+const app = require('express')();
+
+const FBAuth = require('./util/fbAuth');
+
+const { getAllScreams, postOneScream } = require('./handlers/screams');
+const { signup, login, uploadImage } = require('./handlers/users');
+
+
+
+
+//scream routes
+app.get('/screams', getAllScreams);
+app.post('/scream', FBAuth, postOneScream);
+
+//users routes
+app.post('/signup', signup);
+app.post('/login', login);
+app.post('/user/image', FBAuth, uploadImage);
+
+
+
+
+exports.api = functions.https.onRequest(app);
---
+++ practise-functions/functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyCsRIaButC1b8hKGc3AHB4zFUkClsCvzJM",
+    authDomain: "practise-58dec.firebaseapp.com",
+    databaseURL: "https://practise-58dec.firebaseio.com",
+    projectId: "practise-58dec",
+    storageBucket: "practise-58dec.appspot.com",
+    messagingSenderId: "640045630822",
+    appId: "1:640045630822:web:0e376138078a3fe0c334a7",
+    measurementId: "G-R1LKQSG5M9"
+  };
