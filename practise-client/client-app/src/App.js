d5988dd55e68d29bef6a00298caa89be9895e5e3
piyush-cosmo <53268607+piyush-cosmo@users.noreply.github.com>
2020-02-06 17:00:54 +0530

    frontend

--- practise-client/client-app/src/App.js
+++ practise-client/client-app/src/App.js
@@ -3,13 +3,28 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+import jwtDecode from 'jwt-decode';
+
 //Components
 import Navbar from './components/Navbar';
+import AuthRoute from './util/AuthRoute';
+
+// Redux
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import { SET_AUTHENTICATED } from './redux/types';
+import { logoutUser, getUserData } from './redux/actions/userActions';
 
 //Pages
 import home from './pages/home';
 import login from './pages/login';
 import signup from './pages/signup';
+
+import axios from 'axios';
+
+
+axios.defaults.baseURL =
+   'https://europe-west1-practise-58dec.cloudfunctions.net/api';
 
 const theme = createMuiTheme({
   palette: {
@@ -27,29 +42,75 @@ const theme = createMuiTheme({
     }
   },
   typography: {
-    useNextVariable: true
-  }
+    useNextVariants: true
+  },
+  form: {
+    textAlign: 'center'
+},
+image: {
+    margin: '20px auto 20px auto'
+},
+pageTitle: {
+    margin: '10px auto 10px auto'
+},
+textField: {
+    margin: '10px auto 10px auto'
+},
+button: {
+    marginTop: 30,
+    position: 'relative'
+},
+customError: {
+    color: 'red',
+    fontSize: '0.8rem',
+    marginTop: 10
+},
+progress: {
+    position: 'absolute'
+}
 });
+
+const token = localStorage.FBIdToken;
+if (token) {
+  const decodedToken = jwtDecode(token);
+  if (decodedToken.exp * 1000 < Date.now()) {
+    store.dispatch(logoutUser());
+    window.location.href = '/login';
+  } else {
+    store.dispatch({ type: SET_AUTHENTICATED });
+    axios.defaults.headers.common['Authorization'] = token;
+    store.dispatch(getUserData());
+  }
+}
+
 
 class App extends Component {
   render() {
   return (
     <MuiThemeProvider theme={theme}>
-      <div className="App">
-      <Router>
-      <Navbar/>
-        <div className="container">          
-          <Switch>
-            <Route exact path="/" component={home}/>
-            <Route exact path="/login" component={login}/>
-            <Route exact path="/signup" component={signup}/>
-          </Switch>
-        </div>
-      </Router>
-    </div>
-    </MuiThemeProvider>
-  );
-}
-}
+      <Provider store={store}>
+        <Router>
+          <Navbar/>
+            <div className="container">          
+              <Switch>
+                <Route 
+                exact path="/" 
+                component={home}/>
+                <AuthRoute 
+                exact path="/login" 
+                component={login} 
+                />
+                <AuthRoute 
+                exact path="/signup" 
+                component={signup} 
+                />
+              </Switch>
+             </div>
+         </Router>
+       </Provider>
+     </MuiThemeProvider>
+   );
+  }
+ }
 
 export default App;
